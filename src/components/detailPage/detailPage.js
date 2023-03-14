82f678da634b0293437da9b82c52580a9d3fed56
Ermi <manermidem@gmail.com>
2023-03-14 11:43:59 +0300

    Modify the files

--- src/components/detailPage/detailPage.js
+++ src/components/detailPage/detailPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DetailPage = () => (
-  <div>
+  <div className="detail">
     <h1>Hello This is DetailPage</h1>
     <Link to="/">Click here for the home page</Link>
   </div>
--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => (
-  <div>
+  <div className="home">
     <h1>Hello This is HomePage. It is Awesome</h1>
     <Link to="/detailPage">Click here for the detailed page</Link>
   </div>
